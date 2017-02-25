fb954691a6fdb9cf7143f1e2bb110dd6ff224cd0
fanzy446 <fanzy446446@gmail.com>
2017-02-25 14:15:29 -0500

    first commit

---
+++ basic_methods.py
@@ -0,0 +1,71 @@
+# load libraries
+import pandas
+from pandas.tools.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# load dataset
+dataset = pandas.read_csv('training_data_mod.csv')
+
+# shape
+print(dataset.shape)
+
+# head
+print(dataset.head(20))
+
+# descriptions
+print(dataset.describe())
+
+# class distribution
+print(dataset.groupby('Resp').size())
+
+# dataset.plot(kind='box', sharex=False, sharey=False)
+# plt.show()
+
+# histograms
+# dataset.hist()
+# plt.show()
+
+# scatter plot matrix
+# scatter_matrix(dataset)
+# plt.show()
+
+# split-out validation dataset
+array = dataset.values
+X = array[:, 2:4]
+Y = array[:, 4]
+validation_size = 0.20
+seed = 7
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+
+# test options and evaluation metric
+seed = 7
+scoring = 'accuracy'
+
+models = []
+models.append(('LR', LogisticRegression()))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC()))
+
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    results.append(cv_results)
+    names.append(name)
+    msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+    print(msg)
