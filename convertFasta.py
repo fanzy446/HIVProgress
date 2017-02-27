fa142e0692312ed63d3e7d2f165f24710e2f8dcd
fanzy446 <fanzy446446@gmail.com>
2017-02-27 14:29:15 -0500

    find motif

--- convertFasta.py
+++ convertFasta.py
@@ -1,17 +1,16 @@
 import pandas
 import math
 
-filePos = open("PR_pos.fasta", "w+")
-fileNeg = open("PR_neg.fasta", "w+")
+filePos = open("pos/PR_pos.fasta", "w+")
+fileNeg = open("neg/PR_neg.fasta", "w+")
 dataset = pandas.read_csv('training_data.csv')
 
 for record in dataset.values:
-	if record[2]:
-		if record[1] == 1:
-			filePos.write(">%d\n" % (record[0]))
-			filePos.write(record[2] + "\n\n")
-		else:
-			fileNeg.write(">%d\n" % (record[0]))
-			fileNeg.write(record[2] + "\n\n")
+	if record[1] == 1:
+		filePos.write(">%d\n" % (record[0]))
+		filePos.write(record[2] + "\n")
+	else:
+		fileNeg.write(">%d\n" % (record[0]))
+		fileNeg.write(record[2] + "\n")
 
 file.close()
