76c9b6ea264d229c4a6b8a312cde9482d9aadcc1
Qian Yuzhong <cuteqianyuzhong@gmail.com>
2017-03-05 23:53:03 -0500

    complete find markers

--- basic_methods.py
+++ basic_methods.py
@@ -2,7 +2,9 @@
 import pandas
 from pandas.tools.plotting import scatter_matrix
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn import model_selection
+from sklearn import preprocessing
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
@@ -12,21 +14,24 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from find_markers import find_markers
+from sklearn.metrics import confusion_matrix
 
 # load dataset
 dataset = pandas.read_csv('training_data_mod.csv')
 
 # shape
-#print(dataset.shape)
+# print(dataset.shape)
 #
 ## head
-#print(dataset.head(20))
+# print(dataset.head(20))
 #
 ## descriptions
-#print(dataset.describe())
+# print(dataset.describe())
 #
 ## class distribution
-#print(dataset.groupby('Resp').size())
+# print(dataset.groupby('Resp').size())
 
 # dataset.plot(kind='box', sharex=False, sharey=False)
 # plt.show()
@@ -46,93 +51,205 @@ X = array[:, 2:4]
 Y = array[:, 4]
 validation_size = 0.20
 seed = 0
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size, random_state=seed)
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X, Y, test_size=validation_size,
+                                                                                random_state=seed)
 
 # test options and evaluation metric
-seed = 7
+# seed = 7
 scoring = 'accuracy'
 
 models = []
-models.append(('LR', LogisticRegression()))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC()))
+# models.append(('LR', LogisticRegression()))
+# models.append(('LDA', LinearDiscriminantAnalysis()))
+# models.append(('KNN', KNeighborsClassifier()))
+# models.append(('CART', DecisionTreeClassifier()))
+# models.append(('NB', GaussianNB()))
+# models.append(('SVM', SVC()))
+models.append(('RF', RandomForestClassifier()))
 
 # evaluate each model in turn
 results = []
 names = []
-print '***************************** human data *****************************'
-for name, model in models:
-    kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    
-    model.fit(X_train, Y_train)
-    predictions = model.predict(X_validation)
-#    print predictions
-    msg = "%s: %f (%f) %f" % (name, cv_results.mean(), cv_results.std(), accuracy_score(Y_validation, predictions))
-    print(msg)
-
-dic = {'-':0, 'A':1, 'T':2, 'C':3, 'G':4, 'Y':5, 'R':6, 'W':7, 'M':8, 'K':9, 'N':10, 'S':11, 'B':12, 'H':13, 'D':14, 'V':15}
-# two sequences
-X2 = array[:, 0:2]
-X2_mod = []
-
-ratio = len(X2[0][1]) / len(X2[0][0])
-for sequences in X2:
-    temp = [ratio * dic[letter] for letter in sequences[0]]
-    temp += [dic[letter] for letter in sequences[1]]
-    X2_mod.append(temp)
-
-seed = 0
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X2_mod, Y, test_size=validation_size, random_state=seed)
-
-print '***************************** virus DNA *****************************'
-for name, model in models:
-    kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
-    results.append(cv_results)
-    names.append(name)
-    
-    model.fit(X_train, Y_train)
-
-    predictions = model.predict(X_validation)
-#    print predictions
-    msg = "%s: %f (%f) %f" % (name, cv_results.mean(), cv_results.std(), accuracy_score(Y_validation, predictions))
-    print(msg)
+# print '***************************** human data *****************************'
+# for name, model in models:
+#    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+#    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+#    results.append(cv_results)
+#    names.append(name)
+#
+#    model.fit(X_train, Y_train)
+#    predictions = model.predict(X_validation)
+##    print predictions
+#    msg = "%s: %f (%f) %f" % (name, cv_results.mean(), cv_results.std(), accuracy_score(Y_validation, predictions))
+#    print(msg)
+#
+dic = {'-': 0, 'A': 1, 'T': 2, 'C': 3, 'G': 4, 'Y': 5, 'R': 6, 'W': 7, 'M': 8, 'K': 9, 'N': 10, 'S': 11, 'B': 12,
+       'H': 13, 'D': 14, 'V': 15}
+## two sequences
+# X2 = array[:, 0:2]
+# X2_mod = []
+#
+# ratio = len(X2[0][1]) / len(X2[0][0])
+# for sequences in X2:
+#    temp = [ratio * dic[letter] for letter in sequences[0]]
+#    temp += [dic[letter] for letter in sequences[1]]
+#    X2_mod.append(temp)
+#
+# seed = 0
+# X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X2_mod, Y, test_size=validation_size, random_state=seed)
+#
+# print '***************************** virus DNA *****************************'
+# for name, model in models:
+#    kfold = model_selection.KFold(n_splits=10, random_state=seed)
+#    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+#    results.append(cv_results)
+#    names.append(name)
+#
+#    model.fit(X_train, Y_train)
+#
+#    predictions = model.predict(X_validation)
+##    print predictions
+#    msg = "%s: %f (%f) %f" % (name, cv_results.mean(), cv_results.std(), accuracy_score(Y_validation, predictions))
+#    print(msg)
 
 # four attributes
 X3 = array[:, 0:4]
 X3_mod = []
 
-ratio = len(X3[0][1]) / len(X3[0][0])
-ratio2 = len(X3[0][1])
-for sequences in X3:
-    temp = [ratio * dic[letter] for letter in sequences[0]]
-    temp += [dic[letter] for letter in sequences[1]]
-    temp += [ratio2 * sequences[2]]
-    temp += [ratio2 * sequences[3]]
-    X3_mod.append(temp)
+# encoding sequence!!!
+
+# ratio = len(X3[0][1]) / len(X3[0][0])
+# ratio2 = len(X3[0][1])
+ratio = 1
+ratio2 = 1
+
+# for sequences in X3:
+#    temp = [ratio * dic[letter] for letter in sequences[0]]
+#    temp += [dic[letter] for letter in sequences[1]]
+#    temp += [ratio2 * sequences[2]]
+#    temp += [ratio2 * sequences[3]]
+#    X3_mod.append(temp)
+len0 = len(X3[0][0])
+len1 = len(X3[0][1])
+
+enc0 = preprocessing.OneHotEncoder(n_values=[len(dic) for i in range(len0)])
+enc1 = preprocessing.OneHotEncoder(n_values=[len(dic) for i in range(len1)])
+
+# seq0 = []
+# seq1 = []
+# for sequences in X3:
+#     temp = [dic[letter] for letter in sequences[0]]
+#     seq0.append(temp)
+#     temp = [dic[letter] for letter in sequences[1]]
+#     seq1.append(temp)
+#
+# enc0.fit(seq0)
+# enc1.fit(seq1)
+#
+# for sequences in X3:
+#     temp = [dic[letter] for letter in sequences[0]]
+#     a0 = enc0.transform([temp]).toarray()
+#     temp = [dic[letter] for letter in sequences[1]]
+#     a1 = enc1.transform([temp]).toarray()
+#     tmp = np.append(a0[0], a1[0])
+#     tmp = np.append(tmp, sequences[2:])
+#     #    tmp = list(a0[0]) + list(a1[0]) + list(sequences[2:])
+#     X3_mod.append(tmp)
 
 seed = 0
-X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X3_mod, Y, test_size=validation_size, random_state=seed)
+X_train, X_validation, Y_train, Y_validation = model_selection.train_test_split(X3, Y, test_size=validation_size,
+                                                                                random_state=seed)
 
+seq0 = []
+seq1 = []
+for sequences in X_train:
+    temp = [dic[letter] for letter in sequences[0]]
+    seq0.append(temp)
+    temp = [dic[letter] for letter in sequences[1]]
+    seq1.append(temp)
+
+enc0.fit(seq0)
+enc1.fit(seq1)
+
+for sequences in X_train:
+    temp = [dic[letter] for letter in sequences[0]]
+    a0 = enc0.transform([temp]).toarray()
+    temp = [dic[letter] for letter in sequences[1]]
+    a1 = enc1.transform([temp]).toarray()
+    tmp = np.append(a0[0], a1[0])
+    tmp = np.append(tmp, sequences[2:])
+    #    tmp = list(a0[0]) + list(a1[0]) + list(sequences[2:])
+    X3_mod.append(tmp)
+
+print X_validation[0]
 print '***************************** four attributes *****************************'
 for name, model in models:
     kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, X_train, Y_train, cv=kfold, scoring=scoring)
+    cv_results = model_selection.cross_val_score(model, X3_mod, Y_train, cv=kfold, scoring=scoring)
     results.append(cv_results)
     names.append(name)
-    
-    model.fit(X_train, Y_train)
-    
-    predictions = model.predict(X_validation)
+
+    #    print model.get_params(True)
+    weights_history = []
+    for i in range(20):
+        model = RandomForestClassifier()
+        model.fit(X3_mod, Y_train)
+        weights_history.append(model.feature_importances_)
+
+    weight = []
+    for index in range(len(weights_history[0])):
+        max = 0
+        min = 1
+        sum = 0
+        for i in range(20):
+            if max < weights_history[i][index]:
+                max = weights_history[i][index]
+            if min > weights_history[i][index]:
+                min = weights_history[i][index]
+            sum += weights_history[i][index]
+        sum = sum - max - min
+        average = sum / 18
+        weight.append(average)
+
+    #    if name == 'RF':
+    #        for weight in model.feature_importances_:
+    #            print weight
+
+    (PR_pos, PR_neg, RT_pos, RT_neg) = find_markers(X3, Y, weight, len0, len1, len(dic))
+    # predictions = model.predict(X_validation)
     #    print predictions
-    msg = "%s: %f (%f) %f" % (name, cv_results.mean(), cv_results.std(), accuracy_score(Y_validation, predictions))
-    print(msg)
+    # c_matrix = confusion_matrix(Y_validation, predictions)
+    print "fengexian"
+    # print c_matrix
+    # msg = "%s: %f (%f) %f" % (name, cv_results.mean(), cv_results.std(), accuracy_score(Y_validation, predictions))
+    # print(msg)
+
+    result = 0
+    for idx, test in enumerate(X_validation):
+        sum_pos = 0
+        sum_neg = 0
+        for pr_pos in PR_pos:
+            if test[0][pr_pos[1]: pr_pos[2] + 1] == pr_pos[0]:
+                sum_pos += 1
+        for pr_neg in PR_neg:
+            if test[0][pr_neg[1]: pr_neg[2] + 1] == pr_neg[0]:
+                sum_neg += 1
+        for rt_pos in RT_pos:
+            if test[1][rt_pos[1]: rt_pos[2] + 1] == rt_pos[0]:
+                sum_pos += 1
+        for rt_neg in RT_neg:
+            if test[1][rt_neg[1]: rt_neg[2] + 1] == rt_neg[0]:
+                sum_neg += 1
+        if sum_neg >= sum_pos:
+            if Y_validation[idx] == 'worse':
+                result += 1
+        else:
+            if Y_validation[idx] == 'better':
+                result += 1
+        print (sum_pos, sum_neg, Y_validation[idx])
+
+    print float(result) / len(X_validation)
+
 
 
 
--- find_markers.py
+++ find_markers.py
@@ -1,53 +1,112 @@
 import operator
-def find_markers(data, Y, weight, len0, len1, len_dic, marker_numbers = 8 ,marker_length = 15):
+
+
+def find_markers(data, Y, weight, len0, len1, len_dic, marker_numbers=8, marker_length=9):
     weight = weight[0:-2]
     new_weight = [sum(weight[i * len_dic: (i + 1) * len_dic]) for i in range(len(weight) / len_dic)]
     weight_seq1 = new_weight[0: len0]
     weight_seq2 = new_weight[len0:]
-    
+
+    sorted_weight_seq1 = [(idx, val) for idx, val in enumerate(weight_seq1)]
+    sorted_weight_seq1.sort(reverse=True, key=operator.itemgetter(1))
+    sorted_weight_seq2 = [(idx, val) for idx, val in enumerate(weight_seq2)]
+    sorted_weight_seq2.sort(reverse=True, key=operator.itemgetter(1))
+
+    print sorted_weight_seq1[:20]
+    print "fenge"
+    print sorted_weight_seq2[:20]
+    print "fenge"
+
     result0 = []
     result1 = []
-    for pos in range(3):
-        mid_result0 = []
-        mid_result1 = []
-        for i in range(pos, len(weight_seq1), marker_length):
-            temp = sum(weight_seq1[i: i + marker_length])
-            mid_result0.append(Node(temp, i, i + marker_length - 1))
-        for i in range(pos, len(weight_seq2), marker_length):
-            temp = sum(weight_seq2[i: i + marker_length])
-            mid_result1.append(Node(temp, i, i + marker_length - 1))
-        mid_result0.sort(reverse = True, key = operator.attrgetter('sum'))
-        mid_result1.sort(reverse = True, key = operator.attrgetter('sum'))
-        result0.append(mid_result0[0: marker_numbers])
-        result1.append(mid_result1[0: marker_numbers])
-        
+    for i in range(len(weight_seq1) - marker_length + 1):
+        temp = sum(weight_seq1[i: i + marker_length])
+        result0.append(Node(temp, i, i + marker_length - 1))
+    for i in range(len(weight_seq2) - marker_length + 1):
+        temp = sum(weight_seq2[i: i + marker_length])
+        result1.append(Node(temp, i, i + marker_length - 1))
+    result0.sort(reverse=True, key=operator.attrgetter('sum'))
+    result1.sort(reverse=True, key=operator.attrgetter('sum'))
+
+    print [(r.sum, r.begin, r.end) for r in result0[:marker_numbers]]
+    # print [(r.sum, r.begin, r.end) for r in result1[:marker_numbers]]
+    print "fenge"
+
+    posSubseq = []
+    posSubseq1 = []
+    negSubseq = []
+    negSubseq1 = []
+
+    for j in range(marker_numbers):
         statistics = {}
+        statistics1 = {}
         for i in range(len(data)):
-            s = data[i][0][mid_result0[0].begin: mid_result0[0].end + 1]
+            s = data[i][0][result0[j].begin: result0[j].end + 1]
+            s1 = data[i][1][result1[j].begin: result1[j].end + 1]
             if s not in statistics:
-                statistics[s] = [0, Y[i]]
-            statistics[s][0] += 1
+                statistics[s] = [0, 0, [], []]
+            if Y[i] == 'better':
+                statistics[s][0] += 1
+                statistics[s][2].append(i)
+            else:
+                statistics[s][1] += 1
+                statistics[s][3].append(i)
+
+            if s1 not in statistics1:
+                statistics1[s1] = [0, 0]
+            if Y[i] == 'better':
+                statistics1[s1][0] += 1
+            else:
+                statistics1[s1][1] += 1
+
         for key, value in statistics.iteritems():
-            if value[0] > 1:
-                print (key, value)
-        print "wo shi fen ge xian"
+            if value[0] > 3 and value[0] * 4 > value[1] * 1.5:
+                posSubseq.append([key, result0[j].begin, result0[j].end])
+            if value[1] > 12 and value[0] * 4 * 1.5 < value[1]:
+                negSubseq.append([key, result0[j].begin, result0[j].end])
+
+        for key, value in statistics1.iteritems():
+            if value[0] > 3 and value[0] * 4 > value[1] * 1.5:
+                posSubseq1.append([key, result1[j].begin, result1[j].end])
+            if value[1] > 4.5 and value[0] * 4 * 1.5 < value[1]:
+                negSubseq1.append([key, result1[j].begin, result1[j].end])
+    # print len(posSubseq1)
+    # posIdx = {}
+    # posIdx1 = {}
+    # for i in range(len(data)):
+    #     if Y[i] == 'better':
+    #         posIdx[i] = []
+    #         posIdx1[i] = []
+    #         for subseq in posSubseq:
+    #             if data[i][0][subseq[1]:subseq[2]+1] == subseq[0]:
+    #                 posIdx[i].append(subseq)
+    #         for subseq in posSubseq1:
+    #             if data[i][1][subseq[1]:subseq[2] + 1] == subseq[0]:
+    #                 posIdx1[i].append(subseq)
+    # for k, v in posIdx.iteritems():
+    #     print (k, v)
+    #     print posIdx1[k]
+    return [posSubseq, posSubseq1, negSubseq, negSubseq1]
 
 
 
-#        print mid_result0[0: 9]
+# print mid_result0[0: 9]
 #    print result
 
 class Node:
     sum = 0
     begin = -1
     end = -1
-    
+
     def __init__(self, sum, begin, end):
         self.sum = sum
         self.begin = begin
         self.end = end
 
-#    def __lt__(self, other):
+    def __str__(self):
+        return "sum: %f, begin: %d, end %d" % (self.sum, self.begin, self.end)
+
+# def __lt__(self, other):
 #        return self.sum < other.sum
 #
 #    def __iter__(self):
